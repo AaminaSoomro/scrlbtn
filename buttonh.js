1d5eb1e37b3cdb2a1283a424b9294b1833b144ad
AaminaSoomro <64416973+AaminaSoomro@users.noreply.github.com>
2020-09-02 22:18:12 -0700

    buttons

---
+++ buttonh.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    location.replace("https://www.businessinsider.com/14-reasons-why-reading-is-good-for-your-health-2016-12#:~:text=When%20we%20read%2C%20not%20only,developing%20Alzheimer's%20later%20in%20life.")
+  }
